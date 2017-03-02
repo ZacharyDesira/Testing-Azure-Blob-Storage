3a68a428a0c221f22b74cb5ee3327907b77e8c32
ZacharyDesira <zachdesira@hotmail.com>
2017-03-02 16:58:28 +0100

    use table storage

--- AzureBlob/AzureBlob/dbConnection.cs
+++ AzureBlob/AzureBlob/dbConnection.cs
@@ -5,13 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
+//using Microsoft.WindowsAzure.Storage.Blob;    USE FOR BLOB
+using Microsoft.WindowsAzure.Storage.Table;
 using System.IO;
 
 namespace AzureBlob
 {
     class dbConnection
     {
+        private static CloudStorageAccount storageAccount;
+        private CloudTableClient tableClient;
+        private CloudTable table;
+        private CustomerEntity customer;
+
+
+        public dbConnection()
+        {
+            storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        }
+
+        public void retrieveContainers()
+        {
+            tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference("People3");
+            customer = new CustomerEntity("Zachary", "Desira");
+            customer.Email = "zachdesira@hotmail.com";
+            customer.PhoneNumber = "123456789";
+
+            TableOperation insertOperation = TableOperation.Insert(customer);
+            table.Execute(insertOperation);
+
+            //if (table.Exists() == true)
+            //{
+            //    createEntity();
+            //}
+            //else
+            //{
+            //    table.CreateIfNotExists();
+            //    retrieveContainers();
+            //}
+        }
+
+        public void createEntity()
+        {
+            customer = new CustomerEntity("Zachary", "Desira");
+            customer.Email = "zachdesira@hotmail.com";
+            customer.PhoneNumber = "123456789";
+
+            TableOperation insertOperation = TableOperation.Insert(customer);
+            table.Execute(insertOperation);
+        }
+
+        /*         AZURE BLOB STORAGE
         private CloudStorageAccount storageAccount;
         private CloudBlobClient blobClient;
         private CloudBlobContainer container;
@@ -49,6 +94,6 @@ namespace AzureBlob
                 blockBlob = container.GetBlockBlobReference("myblob");
                 blockBlob.UploadFromStream(fileStream);
             }   
-        }
+        }*/
     }
 }
